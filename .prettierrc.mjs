9411d1321c57f650b187db0105ad23a171641c9c
Naya Singhania <naya.singhania@gmail.com>
2024-10-05 23:09:38 -0700

    redo homepage ui + add 404.astro

---
+++ .prettierrc.mjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+export default {
+  tabWidth: 2,
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,17 @@
 import { Button } from "@/components/ui/button";
 
 export function NavBar() {
-    return (
-        <div className="flex justify-center gap-[10px] py-2">
-            <a href="/">
-                <Button variant="link">Home</Button>
-            </a>
-            <a href="/about">
-                <Button variant="link">About</Button>
-            </a>
-            <a href="/projects">
-                <Button variant="link">Projects</Button>
-            </a>
-        </div>
-    );
+  return (
+    <div className="flex justify-center gap-[10px] py-2">
+      <a href="/">
+        <Button variant="link">Home</Button>
+      </a>
+      <a href="/about">
+        <Button variant="link">About</Button>
+      </a>
+      <a href="/projects">
+        <Button variant="link">Projects</Button>
+      </a>
+    </div>
+  );
 }
