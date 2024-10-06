cdea8db19b7917751b6012de47a6082973b65a94
Naya Singhania <naya.singhania@gmail.com>
2024-10-05 21:21:33 -0700

    Update website configuration files

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://naya.singhania.com",
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+});
