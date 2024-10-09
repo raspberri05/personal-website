8a3520fbc1258bbd9f1df501b45a825da642b8d9
Naya Singhania <naya.singhania@gmail.com>
2024-10-08 21:28:32 -0700

    fix signin redirect

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -28,5 +28,5 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
   cookies.set("sb-refresh-token", refresh_token, {
     path: "/",
   });
-  return redirect("/dashboard");
+  return redirect("/admin");
 };
