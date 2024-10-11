7f8278d6e8a56beae9660330e8b293b3c131c887
Naya Singhania <naya.singhania@gmail.com>
2024-10-11 11:40:07 -0700

    further image optimization

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ export default [
   {
     rules: {
       // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
+      "astro/no-set-html-directive": "error",
     },
   },
 ];
